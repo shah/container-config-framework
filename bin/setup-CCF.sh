3570a6cf84a70e4b6abffe32ad511249aae30f68
Shahid Shah <shahid@shah.org>
2018-10-17 20:51:03 -0400

    Reorganized for improved structure

--- bin/setup-CCF.sh
+++ bin/setup-CCF.sh
@@ -7,16 +7,14 @@
 # TODO: only create jsonnet symlink if jsonnet is not already present
 
 export CCF_SRC_REPO_URL=https://github.com/shah/container-config-framework
-export CCF_INSTALL_PATH="${CCF_INSTALL_PATH:-/opt/container-config-framework}"
-export CCF_COMPONENT_NAME="${CCF_COMPONENT_NAME:-container-conf}"
+export CCF_HOME="${CCF_HOME:-/opt/container-config-framework}"
 
 sudo apt install make git jq
 
-sudo mkdir -p $CCF_INSTALL_PATH
-sudo git clone $CCF_SRC_REPO_URL $CCF_INSTALL_PATH
-sudo ln -s $CCF_INSTALL_PATH/bin/jsonnet-v0.11.2 /usr/bin/jsonnet
-sudo ln -s $CCF_INSTALL_PATH/etc /etc/$CCF_COMPONENT_NAME
-sudo ln -s $CCF_INSTALL_PATH/lib /usr/lib/$CCF_COMPONENT_NAME
+sudo mkdir -p $CCF_HOME
+sudo git clone $CCF_SRC_REPO_URL $CCF_HOME
+sudo ln -s $CCF_HOME/bin/ccfmake /usr/bin/ccfmake
+sudo ln -s $CCF_HOME/bin/jsonnet-v0.11.2 /usr/bin/jsonnet
 
-cd /usr/lib/$CCF_COMPONENT_NAME
+cd $CCF_HOME/lib/$CCF_COMPONENT_NAME
 make check-dependencies
