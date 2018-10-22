ec96f7d781b70d4742f8116edf8cc0e6430f59b0
Shahid Shah <shahid@shah.org>
2018-10-22 11:04:47 -0400

    Add color coding

--- bin/generate-facts.sh
+++ bin/generate-facts.sh
@@ -13,6 +13,10 @@
 
 DEST_PATH_RELATIVE=`realpath --relative-to="$CONTAINER_DEFN_HOME" "$DEST_PATH"`
 DEST_FILE_EXTN=.ccf-facts.json
+GREEN=`tput -Txterm setaf 2`
+YELLOW=`tput -Txterm setaf 3`
+WHITE=`tput -Txterm setaf 7`
+RESET=`tput -Txterm sgr0`
 
 if [ ! -d "$DEST_PATH" ]; then
     echo "A CCF container definition facts destination directory path is expected as DEST_PATH."
@@ -26,12 +30,12 @@ logInfo() {
 }
 
 osqueryFactsSingleRow() {
-    logInfo "Running osQuery single row, saving to $DEST_PATH_RELATIVE/$1$DEST_FILE_EXTN: $2"
+    logInfo "Running osQuery single row, saving to ${YELLOW}$DEST_PATH_RELATIVE/$1$DEST_FILE_EXTN${RESET}: ${GREEN}$2${RESET}"
 	osqueryi --json "$2" | jq '.[0]' > $DEST_PATH/$1$DEST_FILE_EXTN
 }
 
 osqueryFactsMultipleRows() {
-    logInfo "Running osQuery multi row, saving to $DEST_PATH_RELATIVE/$1$DEST_FILE_EXTN: $2"
+    logInfo "Running osQuery multi row, saving to ${YELLOW}$DEST_PATH_RELATIVE/$1$DEST_FILE_EXTN${RESET}: ${GREEN}$2${RESET}"
 	osqueryi --json "$2" > $DEST_PATH/$1$DEST_FILE_EXTN
 }
 
@@ -40,17 +44,17 @@ shellEvalFacts() {
 	touch $destFile
 	existingValues=$(<$destFile)
 	if [ -z "$existingValues" ]; then
-	    logInfo "Running shell eval, saving to $DEST_PATH_RELATIVE/$1$DEST_FILE_EXTN: $2 $3"
+	    logInfo "Running shell eval, saving to ${YELLOW}$DEST_PATH_RELATIVE/$1$DEST_FILE_EXTN${RESET}: ${WHITE}$2${RESET} ${GREEN}$3${RESET}"
 		existingValues="{}"
 	else
-	    logInfo "Running shell eval, appending to $DEST_PATH_RELATIVE/$1$DEST_FILE_EXTN: $2 $3"
+	    logInfo "Running shell eval, appending to ${YELLOW}$DEST_PATH_RELATIVE/$1$DEST_FILE_EXTN${RESET}: ${WHITE}$2${RESET} ${GREEN}$3${RESET}"
 	fi
 	textValue=`eval $3`;	
 	echo $existingValues | jq --arg key "$2" --arg value "$textValue" '. + {($key) : $value}' > $destFile
 }
 
 generateFacts() {
-	logInfo "Generating facts from $1 into $DEST_PATH_RELATIVE using JSONNET_PATH $JSONNET_PATH"
+	logInfo "Generating facts from ${YELLOW}$1${RESET} into ${YELLOW}$DEST_PATH_RELATIVE${RESET} using JSONNET_PATH ${GREEN}$JSONNET_PATH${RESET}"
 	jsonnet $1 | jq -r '.osQueries.singleRow[] | "osqueryFactsSingleRow \(.name) \"\(.query)\""' | source /dev/stdin
 	jsonnet $1 | jq -r '.osQueries.multipleRows[] | "osqueryFactsMultipleRows \(.name) \"\(.query)\""' | source /dev/stdin
 	jsonnet $1 | jq -r '.shellEvals[] | "shellEvalFacts \(.name) \(.key) \"\(.evalAsTextValue)\""' | source /dev/stdin
@@ -61,7 +65,7 @@ IFS=':' read -ra FF <<< "$CCF_FACTS_FILES"
      if [ -f "$ff" ]; then
          generateFacts "$ff"
      else
-         logInfo "Skipping facts file $ff from CCF_FACTS_FILES, does not exist."
+         logInfo "Skipping facts file ${YELLOW}$ff${RESET} from CCF_FACTS_FILES, does not exist."
      fi
  done
 
@@ -84,4 +88,4 @@ jsonnet --ext-str CCF_VERSION=$CCF_VERSION \
 		--output-file $DEST_PATH/$CONTEXT_FACTS_GENERATED_FILE \
 		$CONTEXT_FACTS_JSONNET_TMPL
 
-logInfo "Generated $CONTEXT_FACTS_GENERATED_FILE from $CONTEXT_FACTS_JSONNET_TMPL"
+logInfo "Generated ${YELLOW}$CONTEXT_FACTS_GENERATED_FILE${RESET} from ${GREEN}$CONTEXT_FACTS_JSONNET_TMPL${RESET}"
